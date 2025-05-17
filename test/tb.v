b9e4585de087a720ab84323413df02b1752d74de
chandann-rvce <chandann.lvs23@rvce.edu.in>
2025-05-17 10:51:41 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -80,6 +80,10 @@ module tb ();
    
     initial
     #800 $finish;
+
+    initial begin
+    $monitor("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b", $time, ui_in, uio_in, uo_out);
+end
    
 endmodule
 
